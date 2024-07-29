13bf4dc9b6ab17bd6139e4fce0038fe45c423ae7
umeshxtha <umesh.xtha123@gmail.com>
2024-07-29 22:09:49 +0545

    new file made

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
-const { readFile } = require("fs");
-const { result } = require("lodash");
+const { flatMap } = require("lodash")
 
-const getText = (path) => {
-  return new Promise((resolve, reject) => {
-    readFile(path, "utf8", (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(data);
-      }
-    });
-  });
-};
+const { readFile,writeFile } = require("fs").promises
+// const { result } = require("lodash");
+
+
+// const getText = (path) => {
+//   return new Promise((resolve, reject) => {
+//     readFile(path, "utf8", (err, data) => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve(data);
+//       }
+//     });
+//   });
+// };
 
 
 // try catch block ma halam haii
 const start = async () => {
     try{
-        const first = await getText('./content/first.txt')
-        const second = await getText('./content/second.txt')
+        const first = await readFile('./content/first.txt','utf8')
+        const second = await readFile('./content/second.txt','utf8')
+        await writeFile(
+          './content/result-mind-grenade.txt', `This is Awesome : ${first} ${second}`,{flag:'a'}
+        )
     console.log(first,second)
     }
     catch(err){
